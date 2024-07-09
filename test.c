38d1f557d103662ee290ddb2637c976736feddc0
ANAMIKA SANJAY <97059168+ANSANJAY@users.noreply.github.com>
2024-07-09 14:03:47 -0700

    Update test.c

--- test.c
+++ test.c
@@ -3,14 +3,13 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <netdb.h>
 #include <pthread.h>
 
 #define NUM_THREADS 4
 #define NUM_REQUESTS 10
-#define HOST "example.com"
-#define PORT 80
-#define REQUEST "GET / HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n"
+#define HOST "localhost"
+#define PORT 8080
+#define REQUEST "GET / HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n"
 
 void waste_time() {
     for (volatile int i = 0; i < 100000000; ++i);
@@ -19,50 +18,46 @@ void waste_time() {
 void make_request() {
     int sockfd;
     struct sockaddr_in server_addr;
-    struct hostent *server;
     char buffer[4096];
-    
+    ssize_t n;
+
     // Create socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
         perror("ERROR opening socket");
         return;
     }
-    
-    // Get server by name
-    server = gethostbyname(HOST);
-    if (server == NULL) {
-        fprintf(stderr, "ERROR, no such host\n");
-        close(sockfd);
-        return;
-    }
-    
+
     // Setup server address
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     server_addr.sin_port = htons(PORT);
-    
+
     // Connect to server
     if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("ERROR connecting");
         close(sockfd);
         return;
     }
-    
+
     // Send request
     if (write(sockfd, REQUEST, strlen(REQUEST)) < 0) {
         perror("ERROR writing to socket");
         close(sockfd);
         return;
     }
-    
+
     // Read response
-    while (read(sockfd, buffer, sizeof(buffer) - 1) > 0) {
-        // Process response (for demonstration, we just print it)
-        // In a real scenario, you would process the response data
+    while ((n = read(sockfd, buffer, sizeof(buffer) - 1)) > 0) {
+        buffer[n] = '\0';  // Null-terminate the buffer
+        printf("%s", buffer);  // Print the response (for demonstration purposes)
     }
-    
+
+    if (n < 0) {
+        perror("ERROR reading from socket");
+    }
+
     // Close socket
     close(sockfd);
 }
